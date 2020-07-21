cb0e9eab51823d942e9086abe5abc5e60d62a3b9
KaunOYu <okaun@mail.ru>
2020-07-21 15:08:22 +0300

    test

--- InstallWebAppTest.sh
+++ InstallWebAppTest.sh
@@ -1,2 +1,5 @@
 cp -f WebAppTest.conf /etc/nginx/conf.d
 systemctl restart nginx
+cp -f WebAppTest.service /etc/systemd/system
+systemctl enable WebAppTest.service
+systemctl restart WebAppTest.service
