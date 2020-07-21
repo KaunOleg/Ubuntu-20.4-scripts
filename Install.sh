7acce4f6d78710dda490dc39a0e3b1ab8e05e6a5
KaunOYu <okaun@mail.ru>
2020-07-21 10:57:43 +0300

    dotnet

--- Install.sh
+++ Install.sh
@@ -26,7 +26,7 @@ echo "Установка DotNet Core 3.1"
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 apt-get update
-apt-get install -y apt-transport-https && apt-get update && apt-get install -y dotnet-sdk-3.1
+apt-get install -y dotnet-sdk-3.1
 apt-get update
-apt-get install -y apt-transport-https && apt-get update && apt-get install -y aspnetcore-runtime-3.1
+apt-get install -y aspnetcore-runtime-3.1
 echo "--------------------OK--------------------" 
